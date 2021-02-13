cb0b2be0209727d129c5154af7a17b82e1d466b7
David Bielik <me@dvdblk.com>
2021-02-13 21:35:48 +0100

    Add Navigation swipe2dismiss anywhere

--- Notifire/common/ViewController/NotifireNavigationController.swift
+++ Notifire/common/ViewController/NotifireNavigationController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class NotifireNavigationController: UINavigationController {
 
+    // MARK: - Properties
     var navigationBarTintColor: UIColor = .compatibleLabel
 
+    private lazy var fullWidthBackGestureRecognizer = UIPanGestureRecognizer()
+
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationBar.titleTextAttributes = TextAttributes.navigationTitle
         view.backgroundColor = .compatibleSystemBackground
         navigationBar.barTintColor = .compatibleSystemBackground
         navigationBar.tintColor = navigationBarTintColor
+
+        setupFullWidthBackGesture()
+    }
+
+    // MARK: - Private
+    private func setupFullWidthBackGesture() {
+        // credits: https://stackoverflow.com/a/60598558/4249857
+        // The trick here is to wire up our full-width `fullWidthBackGestureRecognizer` to execute the same handler as
+        // the system `interactivePopGestureRecognizer`. That's done by assigning the same "targets" (effectively
+        // object and selector) of the system one to our gesture recognizer.
+        guard
+            let interactivePopGestureRecognizer = interactivePopGestureRecognizer,
+            let targets = interactivePopGestureRecognizer.value(forKey: "targets")
+        else {
+            return
+        }
+
+        fullWidthBackGestureRecognizer.setValue(targets, forKey: "targets")
+        fullWidthBackGestureRecognizer.delegate = self
+        view.addGestureRecognizer(fullWidthBackGestureRecognizer)
     }
 }
 
@@ -26,5 +50,13 @@ extension NotifireNavigationController: Reselectable {
         guard viewControllers.count > 1 else { return false }
         popToRootViewController(animated: animated)
         return true
+    }
+}
+
+extension NotifireNavigationController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let isSystemSwipeToBackEnabled = interactivePopGestureRecognizer?.isEnabled == true
+        let isThereStackedViewControllers = viewControllers.count > 1
+        return isSystemSwipeToBackEnabled && isThereStackedViewControllers
     }
 }
