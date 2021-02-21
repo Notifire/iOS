01c5c5bc76a8077ae6f189abab159b33081d27d8
David Bielik <me@dvdblk.com>
2021-02-21 04:26:36 +0100

    Update AppRevealing anim

--- Notifire/Util/Protocol/Revealing.swift
+++ Notifire/Util/Protocol/Revealing.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 struct AppRevealAnimation {
-    static let delay: TimeInterval = 0.5
-    static let smallScaleUpDuration: TimeInterval = 0.26
-    static let scaleDownDuration: TimeInterval = 0.40
-    static let finalDuration: TimeInterval = 0.38
+    static let delay: TimeInterval = 0.2
+    static let scaleDownDuration: TimeInterval = 0.28
+    static let finalDuration: TimeInterval = 0.32
 }
 
 protocol AppRevealing {
@@ -33,6 +32,8 @@ extension AppRevealing where Self: UIViewController {
     }
 
     func revealContent(completion: (() -> Void)? = nil) {
+        view.transform = CGAffineTransform.identity.scaledBy(x: 1.2, y: 1.2)
+
         // Create a new UIWindow for the app reveal in order to display the animation over any Deeplink induced VC / Alert VC
         let splashVC = UIViewController()
         splashVC.view.backgroundColor = .clear
@@ -45,31 +46,28 @@ extension AppRevealing where Self: UIViewController {
         let splashView = SplashView()
         splashVC.view.add(subview: splashView)
         splashView.embed(in: splashVC.view)
-        let smallScaleUpAnimator = UIViewPropertyAnimator(duration: AppRevealAnimation.smallScaleUpDuration, controlPoint1: CGPoint(x: 0.07, y: 0.81), controlPoint2: CGPoint(x: 0.83, y: 0.96)) {
-            splashView.iconImageView.transform = CGAffineTransform.identity.scaledBy(x: 1.08, y: 1.08)
+        let scaleDownAnimator = UIViewPropertyAnimator(duration: AppRevealAnimation.scaleDownDuration, curve: .easeOut) {
+            splashView.iconImageView.transform = CGAffineTransform.identity.scaledBy(x: 0.84, y: 0.84)
         }
-        smallScaleUpAnimator.addCompletion { _ in
-            let scaleDownAnimator = UIViewPropertyAnimator(duration: AppRevealAnimation.scaleDownDuration, dampingRatio: 0.9) {
-                splashView.iconImageView.transform = CGAffineTransform.identity.scaledBy(x: 0.6, y: 0.6)
-            }
-            scaleDownAnimator.addCompletion { _ in
-                UIView.animateKeyframes(withDuration: AppRevealAnimation.finalDuration, delay: 0, options: [.calculationModePaced], animations: {
-                    UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1, animations: {
-                        splashView.iconImageView.transform = CGAffineTransform.identity.scaledBy(x: 14, y: 14)
-                    })
-                    UIView.addKeyframe(withRelativeStartTime: 0.36, relativeDuration: 0.64, animations: {
-                        splashView.alpha = 0
-                        splashVC.view.transform = .identity
-                    })
-                }, completion: { [unowned self] finished in
-                    guard finished else { return }
-                    splashWindow.dismiss()
-                    completion?()
-                    _ = self.customRevealContentCompletion()
+        scaleDownAnimator.addCompletion { _ in
+            let delay: TimeInterval = 0.2
+            UIView.animateKeyframes(withDuration: AppRevealAnimation.finalDuration, delay: 0, options: [UIView.KeyframeAnimationOptions(animationOptions: .curveEaseIn)], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.8, animations: {
+                    splashView.iconImageView.transform = CGAffineTransform.identity.scaledBy(x: 14, y: 14)
                 })
-            }
-            scaleDownAnimator.startAnimation()
+                UIView.addKeyframe(withRelativeStartTime: delay, relativeDuration: 0.5, animations: {
+                    splashView.alpha = 0
+                })
+            }, completion: { [unowned self] finished in
+                guard finished else { return }
+                splashWindow.dismiss()
+                completion?()
+                _ = self.customRevealContentCompletion()
+            })
+            UIView.animate(withDuration: AppRevealAnimation.finalDuration * (1 - delay), delay: AppRevealAnimation.finalDuration * delay + 0.04, options: [.curveEaseOut], animations: {
+                self.view.transform = .identity
+            }, completion: nil)
         }
-        smallScaleUpAnimator.startAnimation(afterDelay: AppRevealAnimation.delay)
+        scaleDownAnimator.startAnimation(afterDelay: AppRevealAnimation.delay)
     }
 }
